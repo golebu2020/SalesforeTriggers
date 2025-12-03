d0bc68b584296337b56ca79c21f59421a0131df2
Chinedu Olebu <cgolebu.dev@gmail.com>
2025-12-03 04:09:21 -0800

    feat: add named slots

--- force-app/main/default/lwc/setterDemoChild/setterDemoChild.js
+++ force-app/main/default/lwc/setterDemoChild/setterDemoChild.js
@@ -1,3 +1,15 @@
-import { LightningElement } from 'lwc';
+import { api, LightningElement } from 'lwc';
 
-export default class SetterDemoChild extends LightningElement {}
+export default class SetterDemoChild extends LightningElement {
+
+    _internalValue;
+
+    @api
+    get publicProperty(){
+        return this._internalValue;
+    }
+
+    set publicProperty(value){
+        this._internalValue = value;
+    }
+}
--- force-app/main/default/lwc/setterDemoParent/setterDemoParent.js
+++ force-app/main/default/lwc/setterDemoParent/setterDemoParent.js
@@ -1,3 +1,10 @@
 import { LightningElement } from 'lwc';
 
-export default class SetterDemoParent extends LightningElement {}
+export default class SetterDemoParent extends LightningElement {
+
+    myValue;
+
+    handleClick(){
+        this.myValue = 'yes';
+    }
+}
